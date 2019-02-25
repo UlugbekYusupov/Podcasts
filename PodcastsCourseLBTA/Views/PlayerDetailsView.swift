17d3e1c5a9b1462e68bf7102c979434e3a68589f
Ulugbek <ulugbek19960902@gmail.com>
2019-02-25 20:48:28 +0900

    Time Slider, Rewind, Fast Forward, and Volume Controls have been fully functionalized uisng IBActions on them

--- PodcastsCourseLBTA/Views/PlayerDetailsView.swift
+++ PodcastsCourseLBTA/Views/PlayerDetailsView.swift
@@ -112,12 +112,11 @@ class PlayerDetailsView: UIView {
     
    
 
-    //MARK:- IBActions and IBoutlets
+    //MARK:- IBoutlets
     
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var currentTimeSlider: UISlider!
-    
     @IBOutlet weak var episodeImageView: UIImageView! {
         didSet {
             episodeImageView.layer.cornerRadius = 5
@@ -139,6 +138,39 @@ class PlayerDetailsView: UIView {
         didSet {
             titleLabel.numberOfLines = 2
         }
+    }
+    
+    //MARK:- IBActions
+    
+    @IBAction func handleCurrentTimeSliderChange(_ sender: Any) {
+        
+        let percentage = currentTimeSlider.value
+        guard let duration = player.currentItem?.duration else {return}
+        let durationInSeconds = CMTimeGetSeconds(duration)
+        let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+        let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: Int32(NSEC_PER_SEC))
+        
+        player.seek(to: seekTime)
+    }
+    
+    @IBAction func handleFastForward(_ sender: Any) {
+        seekToCurrentTime(delta: 15)
+    }
+    
+    @IBAction func handleRewind(_ sender: Any) {
+        seekToCurrentTime(delta: -15)
+    }
+    
+    fileprivate func seekToCurrentTime(delta: Int64) {
+        
+        let fifteenSeconds = CMTimeMake(value: delta, timescale: 1)
+        let seekTime = CMTimeAdd(player.currentTime(), fifteenSeconds)
+        
+        player.seek(to: seekTime)
+    }
+    
+    @IBAction func handleVolumeChange(_ sender: UISlider) {
+        player.volume = sender.value
     }
     
     @IBAction func handledDismiss(_sender: Any){
