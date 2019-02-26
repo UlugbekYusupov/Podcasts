ea7350d04bb297cc5ad519438363d1f43a18f551
Ulugbek <ulugbek19960902@gmail.com>
2019-02-26 18:15:29 +0900

    Maximizing and Minimizing PlayerDetailsView with Animation

--- PodcastsCourseLBTA/Views/PlayerDetailsView.swift
+++ PodcastsCourseLBTA/Views/PlayerDetailsView.swift
@@ -67,6 +67,10 @@ class PlayerDetailsView: UIView {
         }
     }
     
+    static func initFromNib() -> PlayerDetailsView {
+        return Bundle.main.loadNibNamed("PlayerDetailsView", owner: self, options: nil)?.first as! PlayerDetailsView
+    }
+    
     deinit {
         print("PlayerDetailsView memory being reclaimed ...")
     }
