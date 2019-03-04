58819b603b65c60993c447b423d2b9f8a270c82a
UlugbekYusupov <ulugbek19960902@gmail.com>
2019-03-04 12:34:18 +0900

    Update UIApplication.swift

--- PodcastsCourseLBTA/Extensions/UIApplication.swift
+++ PodcastsCourseLBTA/Extensions/UIApplication.swift
@@ -3,14 +3,13 @@
 //  PodcastsCourseLBTA
 //
 //  Created by Ulugbek Yusupov on 3/4/19.
-//  Copyright © 2019 Brian Voong. All rights reserved.
+//  Copyright © 2019 Ulugbek Yusupov. All rights reserved.
 //
 
 import UIKit
 
 extension UIApplication {
     static func mainTabBarController() -> MainTabBarController? {
-        
         return shared.keyWindow?.rootViewController as? MainTabBarController
     }
 }
