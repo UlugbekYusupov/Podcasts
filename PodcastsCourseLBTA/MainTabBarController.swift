3b61e65ed478adb3c3e53ffa69790b1583fe8d55
Ulugbek <ulugbek19960902@gmail.com>
2019-03-04 12:06:55 +0900

    fixed some bugs of UIPanGestureRecognizer

--- PodcastsCourseLBTA/MainTabBarController.swift
+++ PodcastsCourseLBTA/MainTabBarController.swift
@@ -27,6 +27,7 @@ class MainTabBarController: UITabBarController {
     @objc func minimizePlayerDetails() {
         
         maximizedTopAnchorConstraint.isActive = false
+        bottomAnchorConstraint.constant = view.frame.height
         minimizedTopAnchorConstraint.isActive = true
         
         UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
@@ -41,13 +42,15 @@ class MainTabBarController: UITabBarController {
     
     func maximizePlayerDetails(episode: Episode?) {
         
+        minimizedTopAnchorConstraint.isActive = false //true
         maximizedTopAnchorConstraint.isActive = true //false
         maximizedTopAnchorConstraint.constant = 0
-        minimizedTopAnchorConstraint.isActive = false //true
+        bottomAnchorConstraint.constant = 0
         
         if episode != nil {
             playerDetailsView.episode = episode
         }
+       
         UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             
             self.view.layoutIfNeeded()
@@ -63,7 +66,8 @@ class MainTabBarController: UITabBarController {
     let playerDetailsView = PlayerDetailsView.initFromNib()
     var maximizedTopAnchorConstraint: NSLayoutConstraint!
     var minimizedTopAnchorConstraint: NSLayoutConstraint!
-
+    var bottomAnchorConstraint: NSLayoutConstraint!
+    
     fileprivate func setupPlayerDetailsView() {
         view.insertSubview(playerDetailsView, belowSubview: tabBar)
     
@@ -72,11 +76,16 @@ class MainTabBarController: UITabBarController {
         
         maximizedTopAnchorConstraint =  playerDetailsView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height)
         maximizedTopAnchorConstraint.isActive = true
+        
+        
+        bottomAnchorConstraint =  playerDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: view.frame.height)
+        
+        bottomAnchorConstraint.isActive = true
+        
         minimizedTopAnchorConstraint = playerDetailsView.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -64)
         //minimizedTopAnchorConstraint.isActive = true
         
         playerDetailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        playerDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         playerDetailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
     
--- PodcastsCourseLBTA/Views/PlayerDetailsView.swift
+++ PodcastsCourseLBTA/Views/PlayerDetailsView.swift
@@ -56,13 +56,16 @@ class PlayerDetailsView: UIView {
         }
     }
 
+    var panGesture: UIPanGestureRecognizer!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapMaximize)))
         
-        addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan
-            )))
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan
+            ))
+        addGestureRecognizer(panGesture)
         
         observePlayerCurrentTime()
         
@@ -78,32 +81,47 @@ class PlayerDetailsView: UIView {
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
         
-        if gesture.state == .began {
-            print("Began")
-        }
-        else if gesture.state == .changed {
-            
-            let translation =  gesture.translation(in: self.superview)
-            self.transform = CGAffineTransform(translationX: 0, y: translation.y)
-            
-            self.miniPlayerView.alpha = 1 + translation.y / 200
-            self.maximizedStackView.alpha = -translation.y / 200
+        if gesture.state == .changed {
+            handlePanChanged(gesture: gesture)
         }
         else if gesture.state == .ended {
-            print("Ended")
+            handlePanEnded(gesture: gesture)
+        }
+    }
+    fileprivate func handlePanChanged(gesture: UIPanGestureRecognizer) {
+        
+        let translation =  gesture.translation(in: self.superview)
+        self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+        
+        self.miniPlayerView.alpha = 1 + translation.y / 200
+        self.maximizedStackView.alpha = -translation.y / 200
+    }
+    
+    fileprivate func handlePanEnded(gesture: UIPanGestureRecognizer) {
+        
+        let translation =  gesture.translation(in: self.superview)
+        let velocity = gesture.velocity(in: self.superview)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                
-                self.transform = .identity
+            self.transform = .identity
+            
+            if translation.y < -200 || velocity.y < 500 {
+                let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
+                mainTabBarController?.maximizePlayerDetails(episode: nil)
+                gesture.isEnabled = false
+            }
+            else {
                 self.miniPlayerView.alpha = 1
                 self.maximizedStackView.alpha = 0
-            })
-        }
+            }
+        })
     }
     
     @objc func handleTapMaximize() {
         let mainTabTarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
-        mainTabTarController?.maximizePlayerDetails(episode: episode)
+        mainTabTarController?.maximizePlayerDetails(episode: nil)
+        panGesture.isEnabled = false
     }
     
     static func initFromNib() -> PlayerDetailsView {
@@ -229,5 +247,6 @@ class PlayerDetailsView: UIView {
     @IBAction func handledDismiss(_sender: Any){
        let mainTabBarController =  UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
         mainTabBarController?.minimizePlayerDetails()
+        panGesture.isEnabled = true
     }
 }
